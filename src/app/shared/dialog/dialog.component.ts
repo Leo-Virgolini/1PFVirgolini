2cc9aff995a822306cabb416aa46bfb4cc1be2cc
Leo <leo.virgolini@gmail.com>
2023-11-27 23:08:45 -0300

    actualizado

--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -15,16 +15,16 @@ export class DialogComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.data?.alumno) {
-      this.titulo = "ALUMNO";
+      this.titulo = "alumno";
       this.datos = this.data.alumno.id + " - " + this.data.alumno.apellido + ", " + this.data.alumno.nombre;
     } else if (this.data?.profesor) {
-      this.titulo = "PROFESOR";
+      this.titulo = "profesor";
       this.datos = this.data.profesor.id + " - " + this.data.profesor.apellido + ", " + this.data.profesor.nombre;
     } else if (this.data?.curso) {
-      this.titulo = "CURSO";
+      this.titulo = "curso";
       this.datos = this.data.curso.id + " - " + this.data.curso.materia;
     } else if (this.data?.inscripcion) {
-      this.titulo = "INSCRIPCION";
+      this.titulo = "inscripción";
       this.datos = this.data.inscripcion.id + " - " + this.data.inscripcion.alumno.apellido + ", " + this.data.inscripcion.alumno.nombre + " - " + this.data.inscripcion.curso.materia;
     }
   }
